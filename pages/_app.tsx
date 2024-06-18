385c417823eb1d18afdf3f051e8b15b1d1cfb094
Mark Magdy <mark.magdy@cyshield.com>
2024-06-18 16:03:48 +0300

    init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Sora } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const sora = Sora({
+  weight: ['400', '700']
+   ,subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component className={sora.className} {...pageProps} />;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,68 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        "2xl": "1400px",
+        "3xl": "1920px",
+      },
+      fontFamily: {
+        sora: ["Sora"],
+      },
+      colors: {
+        main:'#052F5F',
+        app: {
+          background: "hsl(var(--app-background))",
+          blue: "hsl(var(--app-blue))",
+          darkblue: "hsl(var(--app-darkblue))",
+          darkerblue: "hsl(var(--app-darkerblue))",
+          lightblue: "hsl(var(--app-lightblue))",
+          green: "hsl(var(--app-green))",
+          softblue: "hsla(var(--app-softblue))",
+          softgray: "hsla(var(--app-softgray))",
+          lightgreen: "hsl(var(--app-lightgreen))",
+          warning: "hsl(var(--app-warning))",
+          red: "hsl(var(--app-red))",
+          lightgrey: "hsl(var(--app-lightgrey))",
+          lightyellow: "hsl(var(--app-lightyellow))",
+          mintgreen: "hsl(var(--app-mintgreen))",
+          lightpurple: "hsl(var(--app-lightpurple))",
+        },
+
+        // shadcn colors
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
     },
   },
   plugins: [],
