c340073c4a922c2e9c0938d8e01b8c39f7e9fb3b
Mark Magdy <mark.magdy@cyshield.com>
2024-06-25 22:21:04 +0300

    fix | seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>AyEstate Awesome Estates</title>
+        <meta
+          name="description"
+          content="Come and check our awesome estates!"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
