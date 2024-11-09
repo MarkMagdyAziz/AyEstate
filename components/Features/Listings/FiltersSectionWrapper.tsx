dd01ff4beed8bf9f9b903b2d14b32872a87c4e55
Mark Magdy <mark.magdy@cyshield.com>
2024-11-09 13:52:54 +0200

    feat | interactivitiy sections

---
+++ components/Features/Listings/FiltersSectionWrapper.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+export const FiltersSectionWrapper: React.FC<{
+  title: string;
+  children: React.ReactNode;
+  lastItem?: boolean;
+}> = ({ children, title, lastItem }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <>
+      <div className="flex justify-between">
+        <h6 className="text-base font-bold leading-[22px]">{title}</h6>
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="h-fit w-fit transform transition-transform duration-300"
+          style={{ transform: isOpen ? "rotate(180deg)" : "rotate(0deg)" }}
+        >
+          <Image
+            width={12}
+            height={12}
+            src="/arrow-up.svg"
+            alt="arrow down icon"
+          />
+        </button>
+      </div>
+      <div
+        className={`mt-3 flex flex-col gap-y-3 overflow-hidden transition-all duration-700 ease-in-out ${isOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}
+      >
+        {children}
+      </div>
+      {!lastItem && <hr className="my-6 mb-6 w-[230px] bg-[#E4E9EE]" />}
+    </>
+  );
+};
+
+export default FiltersSectionWrapper;
