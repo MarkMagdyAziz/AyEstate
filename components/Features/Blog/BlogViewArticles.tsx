8f84ce6887a6ee0c422485c3b97649423c826098
Mark Magdy <mark.magdy@cyshield.com>
2024-12-09 00:55:47 +0200

    feat | recently blogs

--- components/Features/Blog/BlogViewArticles.tsx
+++ components/Features/Blog/BlogViewArticles.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/app/_lib/clsx";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -7,11 +8,19 @@ interface Props {
   className?: string;
   title: string;
   paragraph: string;
+  buttonClassName?: string;
+  paragraphClassName?: string;
 }
 
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
-const BlogViewArticles: React.FC<Props> = ({ className, paragraph, title }) => {
+const BlogViewArticles: React.FC<Props> = ({
+  className,
+  paragraph,
+  title,
+  buttonClassName,
+  paragraphClassName,
+}) => {
   return (
     <div
       className={cn(
@@ -23,18 +32,25 @@ const BlogViewArticles: React.FC<Props> = ({ className, paragraph, title }) => {
         <h3 className="text-base font-bold leading-[20px] text-[#1B2124] lg:text-[32px] lg:leading-[41.6px]">
           {title}
         </h3>
-        <p className="text-xs leading-[20px] text-[#7F879E] lg:text-lg">
+        <p
+          className={cn(
+            "text-xs leading-[20px] text-[#7F879E] lg:text-lg",
+            paragraphClassName,
+          )}
+        >
           {paragraph}
         </p>
       </div>
-      <button
+      <Link
+        href="/blog/recently-articles"
         className={cn(
           "max-h-fit min-w-fit rounded-xl border border-[#B7E08A] bg-transparent px-3 py-[6px] text-sm font-medium text-[#111111] lg:border-[#111111] lg:text-lg lg:font-semibold",
           plusJakartaSans.className,
+          buttonClassName,
         )}
       >
         View All
-      </button>
+      </Link>
     </div>
   );
 };
--- components/Features/Blog/TipCard.tsx
+++ components/Features/Blog/TipCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/app/_lib/clsx";
 import Image from "next/image";
 import React from "react";
 
@@ -9,13 +10,20 @@ interface Props {
   paragraph: string;
   imageSrc: string;
 }
-const TipCard: React.FC<Props> = ({ date, paragraph, title, imageSrc }) => {
+const TipCard: React.FC<Props> = ({
+  date,
+  paragraph,
+  title,
+  imageSrc,
+  className,
+}) => {
   return (
     <div>
       <div
-        className={
-          "relative mx-auto min-h-[260px] w-full lg:min-h-[300px] lg:w-[394px]"
-        }
+        className={cn(
+          "relative mx-auto min-h-[260px] w-full lg:min-h-[300px] lg:w-[394px]",
+          className,
+        )}
       >
         <Image
           alt={title}
@@ -25,12 +33,12 @@ const TipCard: React.FC<Props> = ({ date, paragraph, title, imageSrc }) => {
           sizes="(max-width: 768px) 335px, (max-width: 1024px) 350px"
         />
       </div>
-      <p className="mt-4 flex items-center justify-start gap-x-7 text-xs text-[#7F879E] lg:gap-x-3 lg:text-lg lg:leading-[23px]">
+      <p className="mt-4 flex items-center justify-start gap-x-7 text-xs capitalize text-[#7F879E] lg:gap-x-3 lg:text-lg lg:leading-[23px]">
         <span>{date}</span>
         <span className="h-[3px] w-[3px] rounded-full bg-[#7F879E]"></span>
         <span>{title}</span>
       </p>
-      <h5 className="mr-6 mt-[6px] text-sm font-bold leading-[18.2px] lg:mt-3 lg:text-2xl lg:font-bold lg:leading-[32px]">
+      <h5 className="mr-6 mt-[6px] text-sm font-bold capitalize leading-[18.2px] lg:mt-3 lg:text-2xl lg:font-bold lg:leading-[32px]">
         {paragraph}
       </h5>
     </div>
